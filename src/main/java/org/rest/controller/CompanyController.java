665b21677b57ddb7eed1c04c154010c6bd550625
Ramesh KC <kcramesh8@gmail.com>
2017-04-13 21:51:56 -0500

    get company info by using id.

--- src/main/java/org/rest/controller/CompanyController.java
+++ src/main/java/org/rest/controller/CompanyController.java
@@ -8,25 +8,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/companies")
 public class CompanyController {
 
     @Autowired
     private CompanyService companyService;
 
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
-        return "home";
-    }
-
-    @RequestMapping(value = "/companies", method = RequestMethod.GET)
     public List<Company> getAllCompanies() {
         List<Company> companies = companyService.getListOfCompany();
         return companies;
     }
 
-    @RequestMapping(value = "/addCompany", method = RequestMethod.POST)
+    @RequestMapping(value = "/", method = RequestMethod.POST)
     public void addCompany(@RequestBody Company company) {
         companyService.addCompany(company);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Company getCompanyById(@PathVariable("id") int id) {
+        Company company = companyService.getCompanyById(id);
+        return company;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteCompanyById(@PathVariable("id") int id) {
+        companyService.deleteCompany(id);
     }
 }
